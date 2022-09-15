46739cd4e8081742bb66d97e574e2a74249a81d2
Tiago de Tarso Raggiotto Gonçalves <97037591+tiagodetarso@users.noreply.github.com>
2022-09-15 18:16:12 -0300

    Update bp2app5.php

--- bp2app5.php
+++ bp2app5.php
@@ -1,26 +1,26 @@
 
 <?php
-	$codigo = $_REQUEST['codigo'];
+$codigo = $_REQUEST['codigo'];
 
-	$servername = "193.123.127.132";
-	$username = "ttrgoncalves";
-	$password = "raggio983";
-	$dbname = "bp2app";
+$servername = "---.---.---.---";
+$username = "xxxxxxxxx";
+$password = "xxxxxxxxxx";
+$dbname = "bp2app";
 
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	$conn->set_charset("utf8");
-	$sqlbusca = "SELECT objeto FROM negocios WHERE codigo = '$codigo'";
-	$result = $conn->query($sqlbusca);
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
+$sqlbusca = "SELECT objeto FROM negocios WHERE codigo = '$codigo'";
+$result = $conn->query($sqlbusca);
 	
-	//echo "<h1>'$result'<h1>";
+//echo "<h1>'$result'<h1>";
 
-	if ($result->num_rows > 0) {
-	  while($row = $result->fetch_assoc()) { 
-	        echo $row['objeto'];     
-	  }
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) { 
+	  echo $row['objeto'];     
+	}
 	} else {
 	  echo "Não há dados";
-	}
+}
 
-	$conn->close();
+$conn->close();
 ?>
